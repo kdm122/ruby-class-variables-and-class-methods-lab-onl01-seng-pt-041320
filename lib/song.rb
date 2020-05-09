51fe5f18be432b74f84d0bd462fa64b90ab7f7e4
Kyle McLean <kyledmclean@gmail.com>
2020-05-09 21:35:36 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,7 +4,11 @@ class Song
   
   attr_accessor :name, :artist, :genre
   
-  def initialize
-    @@count 
+  def initialize(name, artist, genre)
+    @@count += 1
+    @name = name
+    @artist = artist
+    @genre = genre
+  end
   
 end
